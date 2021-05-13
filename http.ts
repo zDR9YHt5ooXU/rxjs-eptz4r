e24e40930f27a471d17b0eae232b61e043fa2d58
zDR9YHt5ooXU <git1@kaiguo.ca>
2021-05-13 15:52:56 -0700

    1 file modified

--- http.ts
+++ http.ts
@@ -1,18 +1,14 @@
-import { Subject } from 'rxjs';
-import { onCancel } from './on-cancel';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 class Http {
   get(vdom: string, data?: any) {
-    const sub = new Subject();
     console.log('request started, vdom:', vdom);
-    setTimeout(() => {
-      sub.next({
-        success: true,
-        vdom,
-        data
-      });
-      sub.complete();
-    }, 300);
-    return sub.asObservable();
+    return of({
+      success: true,
+      vdom,
+      data
+    }).pipe(delay(300));
   }
 }
 
